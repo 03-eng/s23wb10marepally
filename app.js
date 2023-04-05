7645750dee5d879715f84f8c15beb14e715466d7
03-eng <teddyreddy789@gmail.com>
2023-04-05 15:52:44 -0500

    adding all files

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var boardRouter = require('./routes/board');
-var Nrouter = require('./routes/notebook');
-var Srouter = require('./routes/seclector');
+var Nrouter = require('./routes/van');
+var Srouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/board',boardRouter);
-app.use('/notebook',Nrouter);
-app.use('/seclector',Srouter);
+app.use('/van',Nrouter);
+app.use('/selector',Srouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
